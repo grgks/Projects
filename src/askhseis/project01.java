bb8a00cefaf23cf7cc2acbf294ec964a7954fb33
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-10-03 14:47:04 +0300

    Testing merge,solving conflicts

--- src/askhseis/project01.java
+++ src/askhseis/project01.java
@@ -11,7 +11,7 @@ public class project01 {
     public static void main(String[] args) {
 
 
-        //testing for creating branch n02
+        //testing for creating branch n03
     try (Scanner in = new Scanner(new File("C:/LOTTO/lotto.txt"));
          PrintStream ps = new PrintStream("C:/LOTTO/lotto.out.txt", StandardCharsets.UTF_8)) {
 
@@ -61,7 +61,7 @@ public class project01 {
         e.printStackTrace();
     }
 }
-//testing merge branch new and deleting branch
+//testing merge branch new, solve conflicts and deleting branch
     public static boolean isEven(int[] arr, int threshold) {
         long evenCount = 0;
 
